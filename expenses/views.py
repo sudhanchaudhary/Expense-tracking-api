01f8410dfcf224370a51adae6ae286142529d798
Madhusudhan Chaudhary <sawuriya007@gmail.com>
2026-06-12 12:27:48 +0545

    fix(views): add missing import (Sum)

--- expenses/views.py
+++ expenses/views.py
@@ -1,6 +1,7 @@
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from django.db.models import Sum
 
 from .models import Category, Expense
 from .serializers import CategorySerializer, ExpenseSerializer
